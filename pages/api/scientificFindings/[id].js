9acac41ca03ba8a3b44fea78394aad08342f982d
Maria <mia.gotcha@proton.me>
2026-06-10 10:14:35 +0200

    implement error handling

--- pages/api/scientificFindings/[id].js
+++ pages/api/scientificFindings/[id].js
@@ -5,12 +5,21 @@ export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
 
-  if (request.method === "GET") {
-    const scientificFinding = await ScientificFinding.findById(id);
+  try {
+    if (request.method === "GET") {
+      const scientificFinding = await ScientificFinding.findById(id);
 
-    if (!scientificFinding) {
-      return response.status(404).json({ status: "Not found" });
+      if (!scientificFinding) {
+        return response
+          .status(404)
+          .json({ status: "Scientific finding not found" });
+      }
+      response.status(200).json(scientificFinding);
     }
-    response.status(200).json(scientificFinding);
+  } catch (error) {
+    return response.status(500).json({
+      status: "Internal Server Error",
+    });
   }
+  response.status(405).json({ status: "Method not allowed" });
 }
--- pages/api/scientificFindings/index.js
+++ pages/api/scientificFindings/index.js
@@ -4,10 +4,15 @@ import ScientificFinding from "@/db/models/ScientificFinding";
 export default async function handler(request, response) {
   await dbConnect();
 
-  if (request.method === "GET") {
-    const scientificFindings = await ScientificFinding.find();
-    return response.status(200).json(scientificFindings);
-  } else {
-    return response.status(405).json({ message: "Mehotd not allowed" });
+  try {
+    if (request.method === "GET") {
+      const scientificFindings = await ScientificFinding.find();
+      return response.status(200).json(scientificFindings);
+    }
+  } catch (error) {
+    return response.status(500).json({
+      status: "Could not fetch scientific findings.",
+    });
   }
+  response.status(405).json({ message: "Method not allowed" });
 }
