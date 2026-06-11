323dcc0667c5d88c5d566dc2a0defd4c306153d4
Maria <mia.gotcha@proton.me>
2026-06-11 14:59:29 +0200

    add basic styling to PersonCard

---
+++ components/PersonCard/PersonCard.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledFigure = styled.figure`
+  width: 100%;
+  margin: 0;
+  border: 1px solid black;
+`;
+
+export const StyledImageWrapper = styled.div`
+  width: 100%;
+`;
+
+export const StyledFigcaption = styled.figcaption`
+  display: flex;
+`;
--- components/PersonCard/index.js
+++ components/PersonCard/index.js
@@ -1,18 +1,29 @@
 import Image from "next/image";
+import {
+  StyledFigure,
+  StyledImageWrapper,
+  StyledFigcaption,
+} from "./PersonCard.styled";
 
 export default function PersonCard({ person }) {
   const { name, birth_year, photo_url } = person;
   return (
-    <section>
-      <Image
-        src={photo_url ? photo_url : "/placeholder.jpg"}
-        alt={`Picture showing ${name}`}
-        fill
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        priority
-      />
-      <p>{name}</p>
-      <p>{birth_year}</p>
-    </section>
+    <StyledFigure>
+      <StyledImageWrapper>
+        <Image
+          src={photo_url ? photo_url : "/placeholder.jpg"}
+          alt={`Picture showing ${name}`}
+          width={100}
+          height={100}
+          style={{ width: "100px", height: "auto" }}
+          priority
+        />
+      </StyledImageWrapper>
+      <StyledFigcaption>
+        <p>
+          {name}, {birth_year}
+        </p>
+      </StyledFigcaption>
+    </StyledFigure>
   );
 }
