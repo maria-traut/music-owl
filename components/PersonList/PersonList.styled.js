bf9fdfd1c75d44074e9a69312e0b0f5e5093dae4
maria-traut <mia.gotcha@proton.me>
2026-06-11 17:02:15 +0200

    Create StyledImage component for PersonList
    Add styled component for images in PersonList.

--- components/PersonList/PersonList.styled.js
+++ components/PersonList/PersonList.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Image from "next/image";
 
 export const StyledPersonList = styled.ul`
   all: unset;
@@ -7,4 +8,9 @@ export const StyledPersonList = styled.ul`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 15px;
+`;
+
+export const StyledImage = styled(Image)`
+width: 100%,
+height: auto;
 `;
