6e970b72ffcf6fceeb992c7839f423112d79040b
Maria <mia.gotcha@proton.me>
2026-06-13 11:55:06 +0200

    add error message and success message with useEffect and timetout

--- components/PersonDetail/index.js
+++ components/PersonDetail/index.js
@@ -1,22 +1,42 @@
 import { useRouter } from "next/router";
+import { useSWRConfig } from "swr";
+import { useState } from "react";
 import {
   StyledFigure,
   StyledImageWrapper,
   StyledFigcaption,
   StyledImage,
 } from "./PersonDetail.styled";
-import { useSWRConfig } from "swr";
+
 
 export default function PersonDetail({ person }) {
-  const router = useRouter;
+  const router = useRouter();
   const { mutate } = useSWRConfig();
   const { name, birth_year, photo_url, _id } = person;
+  const [confirmDeleteMode, setConfirmDeleteMode] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+ useEffect(() => {
+    if (!success) return;
+    const successMessageTimer = setTimeout(() => {
+      setSuccess(false);
+      router.push("/people");
+    }, 3000);
+    return () => {
+      clearTimeout(successMessageTimer);
+    };
+  }, [success]);
+
 
   async function handlePersonDelete() {
     const response = await fetch(`/api/people/${_id}`, { method: "DELETE" });
     if (response.ok) {
+      setSuccess(true);
       mutate("/api/people");
-      router.push("/people");
+    }
+    if (!response.ok) {
+      setError(true);
     }
   }
   return (
@@ -33,13 +53,36 @@ export default function PersonDetail({ person }) {
           {name}, {birth_year}
         </StyledFigcaption>
       </StyledFigure>
-      <button
-        type="button"
-        aria-label="Remove person"
-        onClick={handlePersonDelete}
-      >
-        ❌
-      </button>
+      {!confirmDeleteMode && (
+        <button
+          type="button"
+          aria-label="Remove person"
+          onClick={() => setConfirmDeleteMode(true)}
+        >
+          ❌
+        </button>
+      )}
+      {confirmDeleteMode && (
+        <>
+          <p>{`Do you really want to remove ${name} from your list?`}</p>
+          <button
+            type="button"
+            aria-label="Do not remove person"
+            onClick={() => setConfirmDeleteMode(false)}
+          >
+            No
+          </button>
+          <button
+            type="button"
+            aria-label="Confirm deletion"
+            onClick={handlePersonDelete} {setSuccess(true)}
+          >
+            Yes
+          </button>
+        </>
+      )}
+      {success && <p>Successfully removed!</p>}
+      {error && <p>An Error Occured</p>}
     </>
   );
 }
