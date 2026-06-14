81c32cc19541c71cc881a32e79bd734123a3a1f4
Maria <mia.gotcha@proton.me>
2026-06-14 22:21:28 +0200

    change default photo url in peron Schema

--- db/models/Person.js
+++ db/models/Person.js
@@ -10,7 +10,7 @@ const personSchema = new Schema({
     min: 1900,
     max: new Date().getFullYear(),
   },
-  photo_url: { type: String, default: "/placeholder-new.jpg" },
+  photo_url: { type: String, default: "/placeholder-new.png" },
 });
 
 const Person = mongoose.models.Person || mongoose.model("Person", personSchema);
