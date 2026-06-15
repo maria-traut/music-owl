9ab48c7df0d554a0389ce71b394be25845a2952b
Maria <mia.gotcha@proton.me>
2026-06-15 08:54:04 +0200

    refine styling on science and people page

--- components/Global/Global.styles.js
+++ components/Global/Global.styles.js
@@ -15,21 +15,41 @@ export const StyledDivider = styled.hr`
   margin: 10px 0;
 `;
 
+export const StyledSection = styled.section`
+  padding: 5px 25px;
+  border-radius: 15px;
+  margin-top: 15px;
+  margin-bottom: 15px;
+`;
+
 export const StyledH1 = styled.h1`
   text-align: center;
   margin-bottom: 1rem;
 `;
 
+export const StyledH2 = styled.h2`
+  color: white;
+  font-size: 1.25rem;
+  font-weight: 600;
+  line-height: 1.3;
+`;
+
 //people
 
-export const StyledSection = styled.section`
-  border: 1px solid black;
-  margin-bottom: 20px;
+export const StyledPeopleSection = styled(StyledSection)`
+  background-color: #1b3a5c;
+  color: white;
 `;
 
 // science
+
 export const StyledScienceMain = styled(StyledMain)`
   display: flex;
   flex-direction: column;
   gap: 16px;
+`;
+
+export const StyledScienceSection = styled(StyledSection)`
+  background-color: #e8a838;
+  color: white;
 `;
--- pages/science.js
+++ pages/science.js
@@ -1,13 +1,17 @@
 import ScientificFindingList from "@/components/ScientificFindingList";
-import { StyledMain, StyledSection } from "../components/Global/Global.styles";
+import {
+  StyledMain,
+  StyledScienceSection,
+  StyledH2,
+} from "../components/Global/Global.styles";
 
 export default function Science() {
   return (
     <StyledMain>
-      <StyledSection>
-        <h2>Scientific Findings</h2>
+      <StyledScienceSection>
+        <StyledH2>Scientific Findings</StyledH2>
         <p>Find out more about how music effects our brain.</p>
-      </StyledSection>
+      </StyledScienceSection>
       <ScientificFindingList />
     </StyledMain>
   );
