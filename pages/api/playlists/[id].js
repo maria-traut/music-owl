cef2e2df2d5e6f23d007b78b9c88f604db95c8a1
Maria <mia.gotcha@proton.me>
2026-06-17 16:25:17 +0200

    create API endpoint playlists/[id], add handler for DELETE request

---
+++ pages/api/playlists/[id].js
@@ -0,0 +1,19 @@
+import dbConnect from "@/db/connect";
+import Playlist from "@/db/models/Playlist";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+
+  try {
+    if (request.method === "DELETE") {
+      await Playlist.findByIdAndDelete(id);
+      return response
+        .status(200)
+        .json({ status: `Playlist ${id} successfully deleted.` });
+    }
+  } catch (error) {
+    return response.status(500).jgon({ status: "Internal Server Error" });
+  }
+  response.status(405).json({ status: "Method not allowed" });
+}
