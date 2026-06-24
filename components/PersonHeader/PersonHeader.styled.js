4d1c85be48920914bdf9a407f743ed07270ff9d9
Maria <mia.gotcha@proton.me>
2026-06-24 15:59:43 +0200

    create PersonDeleteDialog component

---
+++ components/PersonHeader/PersonHeader.styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { StyledMenuWrapper } from "../Global/Global.styles";
+
+export const StyledPersonHeaderMenuWrapper = styled(StyledMenuWrapper)`
+  position: relative;
+  top: auto;
+  right: auto;
+  margin-left: auto;
+`;
+
+export const StyledPersonHeader = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 14px;
+  margin: 1rem 0 1.5rem 0;
+`;
+
+export const StyledColorAvatar = styled.span`
+  width: 44px;
+  height: 44px;
+  border-radius: var(--radius-element);
+  background: ${({ $color }) => $color};
+  flex-shrink: 0;
+`;
+
+export const StyledPersonName = styled.h3`
+  font-weight: 500;
+  margin: 0;
+`;
+
+export const StyledPersonYear = styled.p`
+  font-size: 12px;
+  margin: 0;
+  white-space: nowrap;
+`;
--- components/PersonHeader/index.js
+++ components/PersonHeader/index.js
@@ -15,7 +15,6 @@ export default function PersonHeader({
   color,
   showMenu,
   setShowMenu,
-
   setActiveMode,
 }) {
   return (
